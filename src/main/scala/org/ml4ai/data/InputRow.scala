df3180ae8f1319219d5db0158ae41c5a5b09cfc7
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2018-09-07 16:00:06 -0700

    "Optimized code"

--- src/main/scala/org/ml4ai/data/InputRow.scala
+++ src/main/scala/org/ml4ai/data/InputRow.scala
@@ -2,6 +2,7 @@ package org.ml4ai.data
 
 import java.io.InputStream
 
+import scala.collection.mutable
 import scala.io.Source
 import scala.collection.mutable.ListBuffer
 case class InputRow(
@@ -25,59 +26,70 @@ case class InputRow(
                    )
 
 object InputRow{
-  def apply(str:String, headers: Array[String]):InputRow = {
+
+  private val listOfSpecificFeatures = Seq("PMCID", "label", "EvtID", "CtxID", "closesCtxOfClass", "context_frequency",
+    "evtNegationInTail", "evtSentenceFirstPerson", "evtSentencePastTense", "evtSentencePresentTense", "sentenceDistance", "dependencyDistance")
+
+  private def allOtherFeatures(headers:Seq[String]): Set[String] = headers.toSet -- (listOfSpecificFeatures ++ Seq(""))
+
+  private def indices(headers:Seq[String]): Map[String, Int] = headers.zipWithIndex.toMap
+
+  def apply(str:String, headers: Seq[String], allOtherFeatures:Set[String], indices:Map[String, Int]):InputRow = {
     // Parse the commas into tokens
-    //val tok = Seq.empty[String]
-
-
     val rowData = str.split(",")
     val sentencePos = rowData(0).toInt
-    val listOfSpecificFeatures = List("PMCID", "label", "EvtID", "CtxID", "closesCtxOfClass", "context_frequency",
-      "evtNegationInTail", "evtSentenceFirstPerson", "evtSentencePastTense", "evtSentencePresentTense", "sentenceDistance", "dependencyDistance")
-    val head = headers.toSet
-    val listS = headers(0) :: listOfSpecificFeatures
-    val setS = listS.toSet
-    val allOtherFeatures = (head -- setS).toList
-
-    var evt_dependencyTails = new ListBuffer[String]
-    var ctx_dependencyTails = new ListBuffer[String]
-
-    for (a <- allOtherFeatures) {
-      val sub = a.substring(11, a.length)
-
-      if ((a contains "evtDepTail_") && (rowData(headers.indexOf(a)).toDouble > 0.0)) {
 
 
-        evt_dependencyTails += sub
-      }
-
-      else if((a contains "ctxDepTail_") && (rowData(headers.indexOf(a)).toDouble > 0.0)) {
-
-        ctx_dependencyTails += sub
-      }
-
-
+    var evt_dependencyTails = new mutable.HashSet[String]
+    var ctx_dependencyTails = new mutable.HashSet[String]
+    
+    allOtherFeatures foreach {
+      case evt:String if evt.startsWith("evtDepTail") =>
+        if(rowData(indices(evt)) != "0.0")
+          evt_dependencyTails += evt.substring(11)
+      case ctx:String if ctx.startsWith("ctxDepTail") =>
+        if(rowData(indices(ctx)) != "0.0")
+          ctx_dependencyTails += ctx.substring(11)
+      case _ => ()
     }
-    val pmcid = rowData(headers.indexOf("PMCID"))
-    val label = rowData(headers.indexOf("label")).toBoolean
-    val evt = rowData(headers.indexOf("EvtID"))
-    val ctx = rowData(headers.indexOf("CtxID"))
-    val closestCtx = rowData(headers.indexOf("closesCtxOfClass")).toDouble
-    val contextFreq = rowData(headers.indexOf("context_frequency")).toDouble
-    val dependencyDistance = rowData(headers.indexOf("dependencyDistance")).toDouble
-    val sentenceDist = rowData(headers.indexOf("sentenceDistance")).toDouble
-    val evtNegationInTail = rowData(headers.indexOf("evtNegationInTail")).toDouble
-    val evtSentenceFirstPerson = rowData(headers.indexOf("evtSentenceFirstPerson")).toDouble
-    val evtSentencePastTense = rowData(headers.indexOf("evtSentencePastTense")).toDouble
-    val evtSentencePresentTense = rowData(headers.indexOf("evtSentencePresentTense")).toDouble
-    InputRow(sentencePos, pmcid, label, evt, ctx, closestCtx, contextFreq, evtNegationInTail, evtSentenceFirstPerson, evtSentencePastTense, evtSentencePresentTense, dependencyDistance,  sentenceDist, ctx_dependencyTails.toSet, evt_dependencyTails.toSet)
+
+    val pmcid = rowData(indices("PMCID"))
+    val label = rowData(indices("label"))
+    val evt = rowData(indices("EvtID"))
+    val ctx = rowData(indices("CtxID"))
+    val closestCtx = rowData(indices("closesCtxOfClass"))
+    val contextFreq = rowData(indices("context_frequency"))
+    val dependencyDistance = rowData(indices("dependencyDistance"))
+    val sentenceDist = rowData(indices("sentenceDistance"))
+    val evtNegationInTail = rowData(indices("evtNegationInTail"))
+    val evtSentenceFirstPerson = rowData(indices("evtSentenceFirstPerson"))
+    val evtSentencePastTense = rowData(indices("evtSentencePastTense"))
+    val evtSentencePresentTense = rowData(indices("evtSentencePresentTense"))
+
+    InputRow(sentencePos,
+      pmcid,
+      label.toBoolean,
+      evt,
+      ctx,
+      closestCtx.toDouble,
+      contextFreq.toDouble,
+      evtNegationInTail.toDouble,
+      evtSentenceFirstPerson.toDouble,
+      evtSentencePastTense.toDouble,
+      evtSentencePresentTense.toDouble,
+      dependencyDistance.toDouble,
+      sentenceDist.toDouble,
+      ctx_dependencyTails.toSet,
+      evt_dependencyTails.toSet)
   }
 
   def fromStream(stream:InputStream):Seq[InputRow] = {
     val source = Source.fromInputStream(stream)
     val lines = source.getLines()
-    val headers = lines.next() split (",")
-    val ret = (lines map (l => InputRow(l, headers))).toList
+    val headers = lines.next() split ","
+    val features = allOtherFeatures(headers)
+    val ixs = indices(headers)
+    val ret = (lines map (l => InputRow(l, headers.toSeq, features, ixs))).toList
     source.close()
     ret
   }
