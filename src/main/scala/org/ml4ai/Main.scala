c5fba8c0d60cb201ca6dad58c6b1c442110b2b89
ShraddhaThumsi <thumsishraddhasatish@gmail.com>
2018-09-12 10:42:16 -0700

    Added function call in Main.scala

--- src/main/scala/org/ml4ai/Main.scala
+++ src/main/scala/org/ml4ai/Main.scala
@@ -5,12 +5,13 @@ import java.util.zip.GZIPInputStream
 
 import data.InputRow
 import data.Balancer
-
+import data.AggregatedRow
 object Main extends App {
 
   val rows = InputRow.fromStream(new GZIPInputStream(getClass.getResourceAsStream("/features.csv.gz")))
   rows foreach println
   val balancedRows = Balancer.balanceByPaper(rows, 1)
-  //balancedRows foreach println
+  val aggregatedRows = AggregatedRow.fromRows(balancedRows)
+  aggregatedRows foreach println
 
 }
